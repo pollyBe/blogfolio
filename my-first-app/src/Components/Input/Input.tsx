449fb9f8bd3b517bd80797d960f3e635df323f20
Polina Belash <polinka.zhuravlinka@gmail.com>
2024-11-29 23:15:59 +0300

    'feat: modified input component'

--- my-first-app/src/Components/Input/Input.tsx
+++ my-first-app/src/Components/Input/Input.tsx
@@ -1,25 +1,51 @@
 import { useState } from 'react';
-import style from './Input.module.scss'
+import style from './Input.module.scss';
 
 interface IInputProps {
-  title?: string,
-  placeholder: string,
-  type: string,
+  title?: string;
+  placeholder: string;
+  type: 'text' | 'e-mail' | 'password' | 'confirmPassword';
 }
 
 const Input = ({ title, placeholder, type }: IInputProps) => {
-  const [text, setText] = useState(placeholder)
+  const [text, setText] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const inputIsValid = (value: string) => {
+    if (type === 'text') {
+      return value.trim().length > 0;
+    }
+    if (type === 'e-mail') {
+      const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailCheck.test(value);
+    }
+    if (type === 'password') {
+      return value.length >= 7;
+    }
+    return true;
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setText(value);
+    setIsError(!inputIsValid(value));
+  };
 
   return (
     <div className={style.inputWrap}>
       <label className={style.label}>{title}</label>
       <input
-        className={ style.inputBox }
+        className={`${style.inputBox} ${isError ? style.error : ''}`}
         type={type}
         value={text}
-        placeholder={placeholder}
-        onChange={(e) => setText(e.target.value)}></input>
-    </div>)
-}
+        placeholder={isFocused ? '' : placeholder}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default Input;
