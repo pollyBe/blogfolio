f42243e6c46d15446b81aff0f291890f4d47d568
Polina Belash <polinka.zhuravlinka@gmail.com>
2024-11-30 01:13:51 +0300

    'feat: adding styles for cards'

--- my-first-app/src/Components/PostCard/PostCard.tsx
+++ my-first-app/src/Components/PostCard/PostCard.tsx
@@ -2,6 +2,8 @@ import Like from '../../assets/Icon-Thumbs-Up.svg?react'
 import DisLike from '../../assets/Icon-Thumbs-Down.svg?react'
 import Save from '../../assets/Icon-Bookmark.svg?react'
 
+import style from './PostCard.module.scss'
+
 interface IProps {
   date: string,
   title: string,
@@ -9,25 +11,22 @@ interface IProps {
   image?: string,
 }
 const PostCard = ({ date, title, description, image }: IProps) => {
-  // function cuttedDescr() {
-  //   description.substring(0, 200)
-  // }
-  return (<div>
-    <div>
-      <div>
-        <div>
+  return (<div className={style.card}>
+    <div className={style.cardWrap}>
+      <div className={style.infoWrap}>
+        <div className={style.date}>
           <p>{date}</p>
         </div>
-        <h3>{title}</h3>
-        <div>
+        <h3 className={style.title}>{title.substring(0, 150)} ...</h3>
+        <div className={style.description}>
           <p>{description.substring(0, 400)} ...</p>
         </div>
       </div>
-      <div>
+      <div className={style.imgWrap}>
         <img src={image} />
       </div>
     </div>
-    <div>
+    <div className={style.postFooter}>
       <div>
         <button type="button"><Like/></button>
         <button type="button"><DisLike/></button>
